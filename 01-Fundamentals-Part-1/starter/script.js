3b4590498e4d46eb0c85704429a03308f5133b88
Gut Jakub <w65539@student.wsiz.edu.pl>
2023-04-02 18:23:57 +0200

    Operator precedence

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -58,6 +58,7 @@ var lastName = "Jonas";
 console.log(lastName);
 */
 
+/*
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2020;
@@ -84,5 +85,17 @@ console.log(ageJonas > ageSarah); // >, <, >=, <=
 console.log(ageSarah >= 18);
 
 const ifFullAge = ageSarah >= 18; // storing a boolean
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
 
 console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10, y = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
