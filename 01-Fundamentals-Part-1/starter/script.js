aba0e42b9a09a7915cbfa23f0a9403feff063176
Gut Jakub <w65539@student.wsiz.edu.pl>
2023-04-06 18:52:06 +0200

    Truthy and Falsy Values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -209,6 +209,7 @@ const markHigherBMI = bmiMark > bmiJohn;
 console.log(bmiMark, bmiJohn, markHigherBMI);
 */
 
+/*
 //type conversion
 const inputYear = "1991";
 console.log(Number(inputYear), inputYear); // converting a string to a number
@@ -230,4 +231,26 @@ console.log("23" / "2");
 let n = "1" + 1; // '11'
 n = n - 1; // 11 - 1 = 10
 console.log(n);
+*/
 
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 0;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log("You should get a job!");
+}
+
+let height;
+if (height) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
