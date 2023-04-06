3a51fb9fdd065c1f97c75419d08e3a4ba35fdabb
Gut Jakub <w65539@student.wsiz.edu.pl>
2023-04-06 19:14:06 +0200

    Logical Operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -257,6 +257,7 @@ if (height) {
 }
 */
 
+/*
 const age = "18";
 if (age === 18) console.log("You just became an adult :D (strict)");
 if (age == 18) console.log("You just became an adult :D (loose)"); // uses type coercion
@@ -276,3 +277,26 @@ if (favourite === 23) {
 }
 
 if (favourite !== 23) console.log("Why not 23?"); // != is a loose version
+*/
+
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+// if (hasDriversLicense && hasGoodVision) {
+//   console.log("Sarah is able to drive!");
+// } else {
+//   console.log("Someone else should drive...");
+// }
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log("Sarah is able to drive!");
+} else {
+  console.log("Someone else should drive...");
+}
