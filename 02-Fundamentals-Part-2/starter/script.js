a7ed1ab3a4d6ebdb151f76ccca3f8e4083557f10
Gut Jakub <w65539@student.wsiz.edu.pl>
2023-04-11 19:21:58 +0200

    Object Methods

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -295,6 +295,7 @@ const jonas = {
 };
 */
 
+/*
 const jonas = {
   firstName: "Jonas",
   lastName: "Schmedtmann",
@@ -335,3 +336,45 @@ console.log(jonas);
 console.log(
   `${jonas.firstName} has ${jonas.friends.length} friends and his best friend is called ${jonas.friends[0]}`
 );
+*/
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  birthYear: 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  hasDriverslicense: true,
+
+  // calcAge: function (birthYear) {
+  //   return 2037 - birthYear;
+  // }
+
+  // calcAge: function () {
+  //   // console.log(this); // 'this' points to the object that calls the method
+  //   return 2037 - this.birthYear;
+  // }
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function () {
+    return `${
+      this.firstName
+    } is a ${this.calcAge()} year old teacher and he has ${
+      this.hasDriverslicense ? "a" : "no"
+    } driver's licence`;
+  },
+};
+
+console.log(jonas.calcAge());
+// console.log(jonas["calcAge"](1991));
+
+console.log(jonas.age);
+
+// Challenge
+// "Jonas is a 46-year old teacher and he has a driver's license"
+
+console.log(jonas.getSummary());
