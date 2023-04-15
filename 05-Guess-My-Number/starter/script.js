100bcf77e57a2f207552ba5a6222dec562a86424
Gut Jakub <w65539@student.wsiz.edu.pl>
2023-04-16 00:05:25 +0200

    Handling Click Events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,12 +1,32 @@
 "use strict";
 
+/*
 console.log(document.querySelector(".message").textContent);
 
-document.querySelector(".message").textContent = "🎉 Correct Number!";
+document.querySelector(".message").textContent = "🎉 Correct Number!"; //? changing the text content 
 // console.log(document.querySelector(".message").textContent);
 
 document.querySelector(".number").textContent = 13;
 document.querySelector(".score").textContent = 10;
 
-document.querySelector(".guess").value = 23;
+document.querySelector(".guess").value = 23; //? changing a value
 console.log(document.querySelector(".guess").value);
+*/
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value); //? using Number to convert a string type to a number type
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔ No number!";
+  }
+});
+
+/* // practicing arrow functions
+document 
+  .querySelector(".check")
+  .addEventListener(
+    "click",
+    (x = () => console.log(document.querySelector(".guess").value))
+  );
+*/
