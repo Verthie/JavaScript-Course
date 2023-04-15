9b17dd90ac2d83efd4e7f7c45703f3e3a817af96
Gut Jakub <w65539@student.wsiz.edu.pl>
2023-04-16 00:52:04 +0200

    Implementing the Game Logic + Own Additions

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -13,20 +13,54 @@ document.querySelector(".guess").value = 23; //? changing a value
 console.log(document.querySelector(".guess").value);
 */
 
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+let highscore = 0;
+document.querySelector(".number").textContent = secretNumber;
+
 document.querySelector(".check").addEventListener("click", function () {
-  const guess = Number(document.querySelector(".guess").value); //? using Number to convert a string type to a number type
+  const guess = Number(document.querySelector(".guess").value); //? using Number() to convert a string type to a number type
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ No number!";
+  } else if (guess === secretNumber) {
+    // Own addition
+    if (highscore < score) {
+      document.querySelector(".message").textContent =
+        "🎉 Correct number! New Highscore!";
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    } else {
+      document.querySelector(".message").textContent = "🎉 Correct number!";
+    }
+  } else if (guess > 20 || guess < 1) {
+    document.querySelector(".message").textContent = "❗Out of range!"; // Own addition
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too high! 📉";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You Lost!";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too low! 📈";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You Lost!";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
 
-/* // practicing arrow functions
-document 
-  .querySelector(".check")
-  .addEventListener(
-    "click",
-    (x = () => console.log(document.querySelector(".guess").value))
-  );
-*/
+// Own addition
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  document.querySelector(".score").textContent = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector(".number").textContent = secretNumber;
+});
