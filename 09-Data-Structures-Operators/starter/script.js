e79bd9fcb1a4600688518aad772c363073fef5a4
Gut Jakub <w65539@student.wsiz.edu.pl>
2023-04-26 21:00:14 +0200

    Sets

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -56,6 +56,38 @@ const restaurant = {
   },
 };
 
+const ordersSet = new Set([
+  "Pasta",
+  "Pizza",
+  "Pizza",
+  "Risotto",
+  "Pasta",
+  "Pizza",
+]);
+console.log(ordersSet);
+
+console.log(new Set("Jonas"));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has("Pizza"));
+console.log(ordersSet.has("Bread"));
+ordersSet.add("Garlic Bread");
+ordersSet.add("Garlic Bread");
+ordersSet.delete("Risotto");
+// ordersSet.clear();
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+const staff = ["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(
+  new Set(["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"]).size
+);
+
+console.log(new Set("jonasschmedtmann").size);
+
 /*
 ////////////////////////////////////////
 // Looping Objects: Object Keys, Values, and Entries
@@ -533,6 +565,7 @@ Lewandowski: 2
 GOOD LUCK 😀
 */
 
+/*
 // 1.
 // My solution
 let goal = 1;
@@ -581,3 +614,4 @@ for (const [team, value] of Object.entries(game.odds)) {
   const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
   console.log(`Odd of victory ${teamStr}: ${value}`);
 }
+*/
