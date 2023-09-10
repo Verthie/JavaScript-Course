89ddd6cc3ed3744ffa8ef5428b7187c7c4a1db26
Gut Jakub <w65539@student.wsiz.edu.pl>
2023-09-10 20:14:49 +0200

    Working With Strings - Part 3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -57,6 +57,63 @@ const restaurant = {
 };
 
 ////////////////////////////////////////
+//: Working With Strings - Part 3
+
+// Split and join
+console.log("a+very+nice+string".split("+"));
+console.log("Jonas Schmedtmann".split(" "));
+
+const [firstName, lastName] = "Jonas Schmedtmann".split(" ");
+
+const newName = ["Mr.", firstName, lastName.toUpperCase()].join(" ");
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(" ");
+  const namesUpper = [];
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase())); //:& Another solution
+  }
+  console.log(namesUpper.join(" "));
+};
+
+capitalizeName("jessica ann smith davis");
+capitalizeName("jonas schmedtmann");
+
+// Padding
+const message = "Go to gate 23!";
+console.log(message.padStart(25, "+"));
+console.log("Jonas".padStart(25, "+").padEnd(35, "+"));
+
+const maskCreditCard = function (number) {
+  const str = number + "";
+  const last = str.slice(-4);
+  return last.padStart(str.length, "*");
+};
+
+console.log(maskCreditCard(75464324));
+console.log(maskCreditCard(43378231754393492));
+console.log(maskCreditCard("2132189489124124214"));
+
+// Repeat
+const message2 = "Bad weather... All Departures Delayed...";
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${"✈️".repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+//:? split() - dzieli stringa na tablicę, elementy oddzielane są podanym znakiem
+//:? join() - łączy elementy tablicy w stringa, elementy oddzielane są podanym znakiem
+//:? padStart() - wypełnia string do wskazanej długości podanym stringiem zaczynając od początku stringa
+//:? padEnd() - wypełnia string do wskazanej długości podanym stringiem zaczynając od końca stringa
+//:? repeat() - powtarza stringa podaną ilość razy
+
+////////////////////////////////////////
 //: Working With Strings - Part 2
 
 /* 
@@ -184,7 +241,9 @@ checkMiddleSeat("3E");
 
 //:? indexOf() - returns the index of an element by looking from start to end
 //:? lastIndexOf() - returns the index of an element by looking from end to start
-//:? slice() - returns a part of a string using the start and end index 
+//:? slice() - returns a part of a string using the start and end index
+
+
 
 */
 
