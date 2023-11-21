c04e4ca8b906937251ed2bbcaee29a723c030a8f
Gut Jakub <w65539@student.wsiz.edu.pl>
2023-11-21 22:58:41 +0100

    The new at method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -76,6 +76,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 /////////////////////////////////////////////////
 
 //: Simple Array Methods
+/* 
 let arr = ["a", "b", "c", "d", "e"];
 
 //:. SLICE
@@ -115,3 +116,21 @@ console.log([...arr, ...arr2]); // To samo
 
 //:. JOIN
 console.log(letters.join(" - "));
+
+*/
+
+//: The new at method
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+//:& at() - zwraca element tablicy o podanym indeksie
+
+// metody pobierania ostatniego elementu tablicy
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log("jonas".at(0));
+console.log("jonas".at(-1));
