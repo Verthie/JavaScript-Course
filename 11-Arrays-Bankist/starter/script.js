edd8b08167797a472e78ad0c736219e9688f6da1
Verth <w65539@student.wsiz.edu.pl>
2023-11-27 21:35:16 +0000

    The filter Method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,7 +61,8 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-//:. Creating DOM Elements
+//: Creating DOM Elements
+
 const displayMovements = function (movements) {
   containerMovements.innerHTML = "";
   //:& innerHTML - zwraca lub ustawia HTML dla danego elementu
@@ -111,7 +112,7 @@ const currencies = new Map([
 ]); 
 */
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -264,6 +265,21 @@ const movementsDescriptions = movements.map(
 );
 console.log(movementsDescriptions);
 */
+
+//: The filter Method
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter((mov) => mov < 0);
+console.log(withdrawals);
 
 //: Coding Challenge #1
 /* 
