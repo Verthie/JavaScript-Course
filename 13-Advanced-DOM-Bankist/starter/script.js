0995afa078567e42e51a182a0e209b7746c2562a
Gut Jakub <w65539@student.wsiz.edu.pl>
2024-03-09 23:25:43 +0100

    Passing arguments to event handlers

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -119,7 +119,7 @@ const handleHover = function (e) {
 
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
-    const siblings = link.closest('.nav').querySelector('.nav__link');
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
 
     siblings.forEach((el) => {
