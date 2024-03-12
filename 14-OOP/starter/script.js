e3b8123d2b28527d10d532b0cbb655daaf64cc2e
Gut Jakub <w65539@student.wsiz.edu.pl>
2024-03-12 17:03:27 +0100

    What is Object-Oriented Programming?

---
+++ 14-OOP/starter/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//:& classes - in object oriented programming (OOP) a class is a blueprint for creating objects, providing initial values for state (member variables or properties), and implementations of behavior (member functions or methods).
+//:& instance - an object created from a class.
+
+//:* Class is a blueprint for making a house, and the house is an instance of the class.
+
+//:? JavaScript does NOT support real classes like other programming languages. JavaScript classes are just a syntax sugar over the existing prototype-based inheritance.
+
+//:& Abstraction - hiding the complexity and only showing the essential features of the object. It helps to reduce the complexity of the system and also improves the maintainability of the system.
+//:& Encapsulation - keeping properties and methods private inside a class so they are not accessible from outside the class, only exposing them through a public interface (API). It's a protective barrier that keeps the data safe within the class (prevent from outside interference and misuse).
+//:& Inheritance - a mechanism that allows a class to inherit properties and methods from another class. It helps to eliminate redundant code.
+//:* For example, admin class can inherit from user class, because admin is also a user.
+//:& Polymorphism - the ability to present the same interface for different data types. It allows a method to do different things based on the object that it is acting upon by allowing a child class to overwrite a method it inherited from its parent class.
