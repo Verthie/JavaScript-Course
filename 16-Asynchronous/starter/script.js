741e095b90bd0ee26d8994a037616d8d232b175b
Gut Jakub <w65539@student.wsiz.edu.pl>
2024-04-26 11:42:21 +0200

    Throwing Errors Manually

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -128,26 +128,66 @@ const getCountryData = function (country) {
 getCountryData('poland');
 */
 
-//: Chaining Promises and Handling Rejected Promises
+//: Chaining Promises, Handling Rejected Promises
+
+// const getCountryData = function (country) {
+//   // Country 1
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(response => {
+//       console.log(response);
+
+//       if (!response.ok) throw new Error(`Country not found (${response.status})`);
+
+//       return response.json();
+//     })
+//     .then(data => {
+//       renderCountry(data[0]);
+
+//       // const neighbour = data[0].borders?.[0];
+//       const neighbour = 'dfshkjdsk'
+
+//       if (!neighbour) return;
+
+//       // Country 2
+//       return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     })
+//     .then(response => {
+//       if (!response.ok) throw new Error(`Country not found (${response.status})`);
+
+//       return response.json()
+//     })
+//     .then(data => renderCountry(data[0], 'neighbour'))
+//     .catch(err => {
+//       console.error(`${err} 💥💥💥`);
+//       renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//       // no matter the result, whether it renders an error (promise rejected) or a country (promise fulfilled), opacity needs to be 1 so it's visible
+//     });
+// };
+
+//: Throwing Errors Manually
+
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
 
 const getCountryData = function (country) {
-  // Country 1
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(
-      response => response.json()
-      // err => alert(err)
-    )
+  getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country not found')
     .then(data => {
       renderCountry(data[0]);
 
       const neighbour = data[0].borders?.[0];
 
-      if (!neighbour) return;
+      if (!neighbour) throw new Error(`No neighbour found!`);
 
-      // Country 2
-      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+      return getJSON(`https://restcountries.com/v3.1/alpha/${neighbour}`, 'Country not found');
     })
-    .then(response => response.json())
     .then(data => renderCountry(data[0], 'neighbour'))
     .catch(err => {
       console.error(`${err} 💥💥💥`);
@@ -155,7 +195,6 @@ const getCountryData = function (country) {
     })
     .finally(() => {
       countriesContainer.style.opacity = 1;
-      // no matter the result, whether it renders an error (promise rejected) or a country (promise fulfilled), opacity needs to be 1 so it's visible
     });
 };
 
