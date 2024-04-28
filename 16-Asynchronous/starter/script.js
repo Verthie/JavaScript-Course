a4c8e48f3005cf6de73ae6fa0f98ae243671c65e
Gut Jakub <w65539@student.wsiz.edu.pl>
2024-04-28 18:53:32 +0200

    Error Handling With try...catch

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -499,7 +499,7 @@ createImage('/img/img-1.jpg')
   .catch(err => console.error(err));
  */
 
-//: Consuming Promises with Async/Await
+//: Consuming Promises with Async/Await & Error Handling With try...catch
 
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
@@ -508,22 +508,39 @@ const getPosition = function () {
 };
 
 const whereAmI = async function () {
-  // Geolocation
-  const pos = await getPosition();
-  const { latitude: lat, longitude: lng } = pos.coords;
+  try {
+    // Geolocation
+    const pos = await getPosition();
+    const { latitude: lat, longitude: lng } = pos.coords;
 
-  // Reverse geocoding
-  const resGeo = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`);
-  const dataGeo = await resGeo.json();
-  console.log(dataGeo);
+    // Reverse geocoding
+    const resGeo = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`);
+    if (!resGeo.ok) throw new Error(`Problem getting location data`);
 
-  // Country data
-  // fetch(`https://restcountries.com/v3.1/name/${country}`).then(res => console.log(res));
-  const res = await fetch(`https://restcountries.com/v3.1/name/${dataGeo.countryName}`);
-  const data = await res.json();
-  console.log(data);
-  renderCountry(data[0]);
+    const dataGeo = await resGeo.json();
+    console.log(dataGeo);
+
+    // Country data
+    // fetch(`https://restcountries.com/v3.1/name/${country}`).then(res => console.log(res));
+    const res = await fetch(`https://restcountries.com/v3.1/name/${dataGeo.countryName}`);
+    if (!res.ok) throw new Error(`Problem getting country`);
+
+    const data = await res.json();
+    console.log(data);
+    renderCountry(data[0]);
+  } catch (err) {
+    console.error(`${err} 💥`);
+    renderError(`💥 ${err.message}`);
+  }
 };
 
 whereAmI();
 console.log('FIRST');
+
+// try {
+//   let y = 1;
+//   const x = 2;
+//   x = 3;
+// } catch (err) {
+//   alert(err.message);
+// }
