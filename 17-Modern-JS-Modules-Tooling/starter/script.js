b7ddb78eb724478714d673b6a42afd7214a99cdf
Gut Jakub <w65539@student.wsiz.edu.pl>
2024-05-02 13:18:56 +0200

    Top-Level await (ES2022)

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -19,3 +19,28 @@ add('bread', 5); // => 5 bread added to cart
 add('apples', 4); // => 4 apples added to cart
 
 console.log(cart); // => [{product: 'pizza', quantity: 2},{...},{...}]
+
+//: Top-Level await
+
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
