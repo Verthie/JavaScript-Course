6bb9a859d7caa5dc2bc1e6d5a13260636efd07b9
Gut Jakub <w65539@student.wsiz.edu.pl>
2024-05-28 17:28:44 +0200

    Listening For load and hashchange Events

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -31,10 +31,15 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+
     // 1) Loading recipe
     renderSpinner(recipeContainer);
 
-    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e8706');
+    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
@@ -151,4 +156,8 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+// showRecipe();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+// window.addEventListener(events, showRecipe);
+// window.addEventListener(events, showRecipe);
